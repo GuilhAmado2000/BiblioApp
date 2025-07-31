079582e59fc720a29eff609d491634eed3571c10
Guilherme Amado <2201754@estg-ipleiria.pt>
2025-07-31 16:38:53 +0100

    Correções

--- src/backend/app/Http/Controllers/AuthController.php
+++ src/backend/app/Http/Controllers/AuthController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function login(Request $request): \Illuminate\Http\JsonResponse
     {
         $credentials = $request->only(['email', 'password']);
 
@@ -20,5 +21,15 @@ class AuthController extends Controller
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60,
         ]);
+    }
+
+    public function logout(Request $request): \Illuminate\Http\JsonResponse
+    {
+        try {
+            JWTAuth::invalidate(JWTAuth::getToken());
+            return response()->json(['message' => 'User successfully signed out'], 200);
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Failed to logout, please try again.'], 500);
+        }
     }
 }
