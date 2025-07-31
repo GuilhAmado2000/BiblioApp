1dcf08c9d51f458b995fc6bbbe3cdb0030a6f0fb
Guilherme Amado <2201754@estg-ipleiria.pt>
2025-07-31 16:39:18 +0100

    Instalação do Vuetify

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -2,13 +2,27 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+//import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import App from './App.vue'
 import router from './router'
 
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
+//app.use(createPinia()).use(piniaPluginPersistedstate)
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
