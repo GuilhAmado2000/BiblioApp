beb7f7602e0a651581fb3e04edfa345c218e4b48
Guilherme Amado <2201754@estg-ipleiria.pt>
2025-08-06 17:27:37 +0100

    Login - Barra de Carregamento

--- src/frontend/src/views/LoginView.vue
+++ src/frontend/src/views/LoginView.vue
@@ -51,43 +51,68 @@ const login = async () => {
 </script>
 
 <template>
-  <v-progress-circular v-if="isLoading" indeterminate color="white" size="20" width="2"></v-progress-circular>
-  <v-container class="fill-height" fluid>
-    <!-- Alinhar cartão horizontalmente para o centro -->
-    <v-row justify="center" align="center">
-      <v-col cols="12" md="5">
-        <v-card class="pa-8" variant="outlined" style="border: 3px dashed #dd8e07;">
-          <!-- Titulo central amarelo de tamanho h4 e a negrito -->
-          <v-card-title class="text-center text-h4 font-weight-bold" style="white-space: normal">
-            Entrar na Bibli@lma
-          </v-card-title>
+  <div class="overlay-container">
+    <!-- Overlay de carregamento -->
+    <div v-if="isLoading" class="loading-overlay">
+      <v-progress-circular indeterminate color="white" size="50" width="5" />
+    </div>
 
-          <!-- Divisor -->
-          <br><v-divider :thickness="3"></v-divider><br>
+    <!-- Conteúdo principal -->
+    <v-container class="fill-height" fluid>
+      <!-- Alinhar cartão horizontalmente para o centro -->
+      <v-row justify="center" align="center">
+        <v-col cols="12" md="5">
+          <v-card class="pa-8" variant="outlined" style="border: 3px dashed #dd8e07;">
+            <!-- Titulo central amarelo de tamanho h4 e a negrito -->
+            <v-card-title class="text-center text-h4 font-weight-bold" style="white-space: normal">
+              Entrar na Bibli@lma
+            </v-card-title>
 
-          <v-form fast-fail @submit.prevent="login">
-            <!-- Campo Username -->
-            <v-text-field v-model="username" label="Nome de utilizador" required></v-text-field>
-            <!-- Campo Password -->
-            <v-text-field v-model="password" label="Senha" type="password" required></v-text-field>
-            <!-- Botão Login -->
-            <v-btn class="login-btn mt-4 font-weight-bold" type="submit" block>Login</v-btn>
-          </v-form>
+            <!-- Divisor -->
+            <br><v-divider :thickness="3"></v-divider><br>
 
-          <!-- Link "Esqueceu a palavra-passe?" -->
-          <v-btn class="mt-4 text-none" variant="text" color="primary" block @click="forgotPassword">Esqueceu a palavra-passe?</v-btn>
+            <v-form fast-fail @submit.prevent="login">
+              <!-- Campo Username -->
+              <v-text-field v-model="username" label="Nome de utilizador" required></v-text-field>
+              <!-- Campo Password -->
+              <v-text-field v-model="password" label="Senha" type="password" required></v-text-field>
+              <!-- Botão Login -->
+              <v-btn class="login-btn mt-4 font-weight-bold" type="submit" block>Login</v-btn>
+            </v-form>
 
-          <br>
-          <v-alert v-if="loginFailed" type="error">
-            <strong>Erro a efetuar login!</strong> Verifique o nome de utilizador e a password.
-          </v-alert>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+            <!-- Link "Esqueceu a palavra-passe?" -->
+            <v-btn class="mt-4 text-none" variant="text" color="primary" block @click="forgotPassword">Esqueceu a palavra-passe?</v-btn>
+
+            <br>
+            <v-alert v-if="loginFailed" type="error">
+              <strong>Erro a efetuar login!</strong> Verifique o nome de utilizador e a password.
+            </v-alert>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <style scoped>
+.overlay-container {
+  position: relative;
+}
+
+.loading-overlay {
+  position: fixed;
+  z-index: 9999;
+  background-color: rgba(0, 0, 0, 0.5); /* fundo escurecido */
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
 .v-card{
   background-color: white;
 }
